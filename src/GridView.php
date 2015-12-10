f65526652be5fa191a0aa73e93fa6f938ff2b009
Memory Clutter <memclutter@gmail.com>
2015-12-10 10:14:28 +0300

    GridView class

---
+++ src/GridView.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace starcode\yii\grid;
+
+class GridView extends \yii\grid\GridView
+{
+    /**
+     * @var string the layout that determines how different sections of the list view should be organized.
+     * The following tokens will be replaced with the corresponding section contents:
+     *
+     * - `{summary}`: the summary section. See [[renderSummary()]].
+     * - `{errors}`: the filter model error summary. See [[renderErrors()]].
+     * - `{items}`: the list items. See [[renderItems()]].
+     * - `{sorter}`: the sorter. See [[renderSorter()]].
+     * - `{pager}`: the pager. See [[renderPager()]].
+     * - `{actions}`: the group actions. See [[renderActions()]].
+     */
+    public $layout = "{summary}\n{actions}\n{items}\n{pager}";
+
+    /**
+     * @inheritdoc
+     */
+    public function renderSection($name)
+    {
+        switch ($name) {
+            case "{actions}":
+                return $this->renderActions();
+            default:
+                return parent::renderSection($name);
+        }
+    }
+
+    /**
+     * Render the group actions.
+     */
+    public function renderActions()
+    {
+
+    }
+}
